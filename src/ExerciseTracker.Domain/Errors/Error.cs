60286f65f4d2e56aa12b1f47aca74db3df8e4f19
nwdorian <neuwirthdorian@gmail.com>
2025-07-09 16:01:32 +0200

    add ErrorType enum to custom errors

--- src/ExerciseTracker.Domain/Errors/Error.cs
+++ src/ExerciseTracker.Domain/Errors/Error.cs
@@ -1,10 +1,10 @@
 namespace ExerciseTracker.Domain.Errors;
 
-public record class Error(string Code, string Message)
+public record class Error(string Code, string Message, ErrorType type)
 {
-    public static readonly Error None = new(string.Empty, string.Empty);
+    public static readonly Error None = new(string.Empty, string.Empty, ErrorType.None);
 
-    public static readonly Error NullValue = new("Error.NullValue", "The specified result value is null.");
+    public static readonly Error NullValue = new("Error.NullValue", "The specified result value is null.", ErrorType.NullValue);
 
-    public static readonly Error ConditionNotMet = new("Error.ConditionNotMet", "The specified condition was not met.");
+    public static readonly Error ConditionNotMet = new("Error.ConditionNotMet", "The specified condition was not met.", ErrorType.ConditionNotMet);
 }
