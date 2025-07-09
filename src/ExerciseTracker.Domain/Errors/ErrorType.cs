a0288b01371fab9cec18f3211d4b70d1ff7bda47
nwdorian <neuwirthdorian@gmail.com>
2025-07-09 15:58:34 +0200

    add ErrorType enum

---
+++ src/ExerciseTracker.Domain/Errors/ErrorType.cs
@@ -0,0 +1,13 @@
+namespace ExerciseTracker.Domain.Errors;
+
+public enum ErrorType
+{
+    None,
+    NullValue,
+    ConditionNotMet,
+    NotCreated,
+    NotFound,
+    NotDeleted,
+    NotUpdated,
+    NotModified
+}
