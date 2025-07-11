71328a8a97d4cf4c6952f5904a6d4a9db96a282b
nwdorian <neuwirthdorian@gmail.com>
2025-07-11 15:49:32 +0200

    add Duration property to Exercise response

--- src/ExerciseTracker.Application/Contracts/Exercises/ExerciseResponse.cs
+++ src/ExerciseTracker.Application/Contracts/Exercises/ExerciseResponse.cs
@@ -2,4 +2,4 @@ using ExerciseTracker.Application.Contracts.Categories;
 
 namespace ExerciseTracker.Application.Contracts.Exercises;
 
-public record class ExerciseResponse(Guid Id, DateTime Start, DateTime End, string Description, CategoryResponse Category);
+public record class ExerciseResponse(Guid Id, DateTime Start, DateTime End, TimeSpan Duration, string Description, CategoryResponse Category);
