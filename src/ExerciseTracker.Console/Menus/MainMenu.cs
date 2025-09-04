50930a482b3a8d01a0d1c48654187e8a891a8bf6
nwdorian <neuwirthdorian@gmail.com>
2025-09-04 17:15:14 +0200

    add main menu

---
+++ src/ExerciseTracker.Console/Menus/MainMenu.cs
@@ -0,0 +1,54 @@
+using Spectre.Console;
+
+namespace ExerciseTracker.Console.Menus;
+
+public class MainMenu
+{
+    private enum MenuOptions
+    {
+        ManageExercises,
+        ManageCategories,
+        Exit
+    }
+
+    public async Task Display()
+    {
+        var exit = false;
+
+        while (!exit)
+        {
+            AnsiConsole.Clear();
+
+            AnsiConsole.Write(
+                new FigletText("Exercise Tracker")
+                    .LeftJustified()
+                    .Color(Color.Yellow)
+            );
+
+            var selection = await AnsiConsole.PromptAsync(
+                new SelectionPrompt<MenuOptions>()
+                    .Title("Select from the menu:")
+                    .UseConverter(GetMenuOptions)
+                    .AddChoices(Enum.GetValues<MenuOptions>())
+            );
+
+            // selection switch
+            // {
+            //     MenuOptions.ManageExercises =>
+            //     MenuOptions.ManageCategories => 
+            //     MenuOptions.Exit => 
+            // };
+        }
+    }
+
+    private static string GetMenuOptions(MenuOptions options)
+    {
+        return options switch
+        {
+            MenuOptions.ManageExercises => "Manage Exercises",
+            MenuOptions.ManageCategories => "Manage Categories",
+            MenuOptions.Exit => "Exit",
+            _ => options.ToString()
+        };
+    }
+}
